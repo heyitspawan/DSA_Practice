df2d58bb3925d3803e3ae51074e424437e5eb975
Pawan Kumar Verma <167613887+heyitspawan@users.noreply.github.com>
2026-08-23 20:35:01 +0530

    Time: 0 ms (100%), Space: 62.9 MB (49.34%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -4,7 +4,7 @@ public:
         int i = 0, j = height.size() - 1;
         int w = 0, area;
         while (i < j) {
-            area = (j - i) * (min(height[j], height[i]));
+            area = (j - i) * min(height[j], height[i]);
             w = max(area, w);
             if (height[i] >= height[j])
                 j--;
