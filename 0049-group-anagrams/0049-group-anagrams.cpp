75691036390a5c087aa058cf679229a65c6818e6
Pawan Kumar Verma <167613887+heyitspawan@users.noreply.github.com>
2026-09-05 15:53:30 +0530

    Time: 15 ms (69.4%), Space: 26.2 MB (42.5%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,26 +1,42 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> res={{strs[0]}};
-
-        for (int i = 1; i < strs.size(); i++) {
-            int flag=0;
-            string s = strs[i];
+        unordered_map<string,vector<string>>mp;
+        for(int i=0;i<strs.size();i++){
+            string s=strs[i];
             sort(s.begin(),s.end());
-            for (int j = 0; j < res.size(); j++) {
-                string t = res[j][0];
-                if(t.size()==s.size()){
-                sort(t.begin(), t.end());
-                if (s == t)
-                 {   res[j].push_back(strs[i]);
-                 flag=1;
-                    break;}}
-            }
-            if(flag==0){
-                res.push_back({strs[i]});
-              //  c++;
-            }
+            mp[s].push_back(strs[i]);
         }
-        return res;
+        vector<vector<string>>ans;
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
+        return ans;
     }
-};
+};// here we used hash map so that we look the word fast in the map 
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         vector<vector<string>> res={{strs[0]}};
+
+//         for (int i = 1; i < strs.size(); i++) {
+//             int flag=0;
+//             string s = strs[i];
+//             sort(s.begin(),s.end());
+//             for (int j = 0; j < res.size(); j++) {
+//                 string t = res[j][0];
+//                 if(t.size()==s.size()){
+//                 sort(t.begin(), t.end());
+//                 if (s == t)
+//                  {   res[j].push_back(strs[i]);
+//                  flag=1;
+//                     break;}}
+//             }
+//             if(flag==0){
+//                 res.push_back({strs[i]});
+//               //  c++;
+//             }
+//         }
+//         return res;
+//     }
+// };
