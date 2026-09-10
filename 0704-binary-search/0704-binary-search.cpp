1b815bb1504f0e6f0999c67de25f4842c0c45c9c
Pawan Kumar Verma <167613887+heyitspawan@users.noreply.github.com>
2026-09-11 01:17:46 +0530

    Time: 0 ms (100%), Space: 31.5 MB (7.19%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -4,9 +4,9 @@ public:
         int l = 0, r = nums.size()-1;
         
         while (l <=r) {
-            if(l==r){
-            if(nums[l]==target) return l;
-        }
+        //     if(l==r){
+        //     if(nums[l]==target) return l;
+        // }
             int m = (l + r) / 2;
             if (target == nums[m])
                 return m;
