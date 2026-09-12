bd39e4afdc7d991e544395e3e6da510a3cff9c14
Pawan Kumar Verma <167613887+heyitspawan@users.noreply.github.com>
2026-09-12 14:34:19 +0530

    Time: 0 ms (100%), Space: 26.2 MB (71.18%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,35 +2,32 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int res = nums[0];
-        for (int i = 1; i < nums.size(); i++) {
-            res ^= nums[i];
+    
+        int l = 0, r = nums.size() - 1;
+        if (nums.size() == 1)
+            return nums[0];
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if(m==0 && nums[m] != nums[m +1]) return nums[m];
+            if(m==nums.size() &&nums[m] != nums[m - 1]) return nums[m];
+            if (nums[m] != nums[m - 1] && nums[m] != nums[m +1])
+                return nums[m];
+            else if (nums[m] == nums[m - 1]) {
+                if ((m-1)%2==0) {
+                    l=m+1;
+
+                } else {
+                    r = m -2;
+                }
+            } else {
+                if (m % 2 == 0) {
+                    l = m + 2;
+                } else {
+                    r= m -1;
+                }
+            }
         }
-        return res;
+        return -1;
     }
-    //     int l = 0, r = nums.size() - 1;
-    //     if (nums.size() == 1)
-    //         return nums[0];
-
-    //     while (l <= r) {
-    //         int m = l + (r - l) / 2;
-    //         if (nums[m] != nums[m - 1] && nums[m] != nums[m +1])
-    //             return nums[m];
-    //         else if (nums[m] == nums[m - 1]) {
-    //             if ((m - l + 1) % 2 != 0) {
-    //                 r = m;
-
-    //             } else {
-    //                 l = m + 1;
-    //             }
-    //         } else {
-    //             if ((r - m) % 2 == 0) {
-    //                 r = m - 1;
-    //             } else {
-    //                 l = m + 1;
-    //             }
-    //         }
-    //     }
-    //     return -1;
-    // }
 };
