f65b3312ec7976335e68abf49e29d1a9d4aed807
Pawan Kumar Verma <167613887+heyitspawan@users.noreply.github.com>
2026-09-22 09:59:09 +0530

    Time: 35 ms (14.31%), Space: 24.4 MB (16.76%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,35 +1,56 @@
 class Solution {
-public:
-    void f(int i, int j, vector<vector<char>>& grid,
-           vector<vector<int>>& visited) {
-        vector<vector<int>> dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        for (int k = 0; k < 4; k++) {
-            int new_i = i + dir[k][0];
-            int new_j = j + dir[k][1];
-            if (new_i >= 0 && new_i < grid.size() && new_j >= 0 &&
-                new_j < grid[0].size() && visited[new_i][new_j] != 1 &&
-                grid[new_i][new_j] == '1') {
-                visited[new_i][new_j] = 1;
-                f(new_i, new_j, grid, visited);
+private:
+    void bfs(int i,int j,vector<vector<char>>& grid, vector<vector<int>>& vis) {
+        vis[i][j] = 1;
+         int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        while (!q.empty()) {
+            int oi = q.front().first;
+            int oj=q.front().second;
+            q.pop();
+            int dr[]={0,-1,0,+1};
+            int dc[]={-1,0,+1,0};
+            for(int k=0;k<4;k++){
+                int ni=oi+dr[k];
+                int nj=oj+dc[k];
+                if(ni>=0&&ni<n&&nj>=0&&nj<m&&grid[ni][nj]=='1'&&vis[ni][nj]!=1){
+                    vis[ni][nj]=1;
+                    q.push({ni,nj});
+                }
             }
+           
         }
     }
 
+
+public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
+        // vector<int> adj[n];
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < m; j++) {
+        //         if (grid[i][j] == '1') {
+        //             adj[i].push_back(j);
+        //             adj[j].push_back(i);
+        //         }
+        //     }
+        // }
+        vector<vector<int>> vis(n, vector<int>(m,-1));
         int res = 0;
-
-        vector<vector<int>> visited(n, vector<int>(m, 0));
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (visited[i][j] == 0 && grid[i][j] == '1') {
-                    visited[i][j] = 1;
-                    res++;
-                    f(i, j, grid, visited);
-                }
+            for(int j=0;j<m;j++){
+
+            
+            if (vis[i][j] == -1 && grid[i][j]=='1') {
+                res++;
+                bfs(i, j,grid, vis);
             }
-        }
+        }}
         return res;
     }
-};
+
+}
+;
